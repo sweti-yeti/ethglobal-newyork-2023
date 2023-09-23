4a68a992c80ff64c9028882b07d9d78aa30641c4
Chief Yeti <yeti@knightwave.gg>
2023-09-23 14:44:50 -0400

    more game client container tweaks

--- frontend/src/app/game.tsx
+++ frontend/src/app/game.tsx
@@ -3,13 +3,13 @@ import Script from 'next/script'
 export default function Game() {
     return(
         <>
-        <canvas id="canvas">
+        <canvas id="canvas" className="block">
 			HTML5 canvas appears to be unsupported in the current browser.
 			Please try updating or use a different browser.
 		</canvas>
-		<div id="status">
-			<div id="status-progress" className="hidden">
-				<div id ="status-progress-inner"></div>
+		<div id="status" className='flex align-items-center justify-center'>
+			<div id="status-progress" className="hidden w-5/6">
+				<div id ="status-progress-inner" className="box-border rounded-e"></div>
 			</div>
 			<div id="status-indeterminate" className="hidden">
 				<div></div>
@@ -24,8 +24,8 @@ export default function Game() {
 			<div id="status-notice" className="m-32 p-32"></div>
 		</div>
  
-        <Script src="./client/runner-onchain.js"/>
-        <Script src="./client/game.js"/>
+        <Script src="./client/runner-onchain.js" strategy='afterInteractive'/>
+        <Script src="./client/game.js" strategy='afterInteractive'/>
         </>
     )
 }

8534170603d26a86b5baf5de85cfb00c22ddb700
Chief Yeti <yeti@knightwave.gg>
2023-09-23 16:38:16 -0400

    frontent tweaks + game export

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -71,13 +71,14 @@ export default function Home() {
 
   
   return (
-    <main className="">
-      <div className="">
-        <h1>Based Account Abstraction</h1>
+    <main className="flex h-screen place-content-center align-middle">
+      <div className="place-content-center align-middle m-auto">
+        <h1 className="">Based Account Abstraction</h1>
         <h2>Connect and Mint your AA powered NFT now</h2>
         {!loading && !address && <button onClick={connect} className="">Connect to Based Web3</button>}
         {loading && <p>Loading Smart Account...</p>}
         {address && <h2>Smart Account: {address}</h2>}
+        {address && <a href="/play">Play</a>}
       </div>
     </main>
   )

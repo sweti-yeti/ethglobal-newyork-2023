f9f94978bab1782cf89abe418e25d462b13c3a5d
Chief Yeti <yeti@knightwave.gg>
2023-09-23 16:40:35 -0400

    leaderboard contract setup

--- contracts/src/Leaderboard.sol
+++ contracts/src/Leaderboard.sol
@@ -3,18 +3,31 @@ pragma solidity ^0.8.13;
 
 contract Leaderboard {
     struct ScoreRecord {
-        address wallet;
         uint64 time;
         uint64 distance;
     }
 
-    mapping(address => string) gameplayLogs;
+    uint64 leaderboardSize = 0;
 
-    function submitScore(uint64 time, uint64 distance, bytes calldata) public {
+    mapping(uint => address) leaderboard;
+    mapping(address => ScoreRecord) scores;
+    mapping(address => bytes) gameplayLogs;
 
+    function submitScore(uint64 time, uint64 distance, bytes memory log) public {
+        ScoreRecord memory current = scores[msg.sender];
+        if(current.time <= time && current.distance <= distance) {
+            scores[msg.sender] = ScoreRecord(time, distance);
+            gameplayLogs[msg.sender] = log;
+        }
+        // Shuffle leaderboard positions
     }
 
-    function getGameplayLog(address wallet) external view returns (string memory) {
+
+    function getGameplayLog(address wallet) external view returns (bytes memory) {
         return gameplayLogs[wallet];
+    }
+    
+    function getScore(address wallet) external view returns (ScoreRecord memory) {
+        return scores[wallet];
     }
 }

095672e49d2457bb48f7305dc7bc3db0b229b908
Chief Yeti <yeti@knightwave.gg>
2023-09-23 23:13:23 -0400

    stub frontend functions

--- frontend/src/app/play/game.tsx
+++ frontend/src/app/play/game.tsx
@@ -3,30 +3,39 @@ import Script from 'next/script'
 import { createContext, useContext, useEffect } from 'react'
 
 interface IGameContext {
-	submitScore: () => Promise<void> | null
+	submitScore: (game_log: String) => Promise<void> | null,
+	getHighscores: () => Promise<void> | null,
+	getPlayerScore: () => Promise<void> | null,
 }
 
 const GameContext = createContext<IGameContext>({
-	submitScore: async () => {
+	submitScore: async (game_log: String) => {
 		console.log("Godot called this!")
-		// Retrieve game log
+		console.log(game_log)
 		// call Cartesi InputBox with game log
 		// poll for outputs
 		// If valid score
 		// 	run voucher
+	},
+	getHighscores: async () => {
+	},
+	getPlayerScore: async () => {
 	}
 })
 
 export default function Game() {
-	let { submitScore } = useContext(GameContext);
+	let { submitScore, getHighscores, getPlayerScore } = useContext(GameContext);
 	useEffect(() => {
 		//@ts-ignore
 		window.submitScore = submitScore;
+		//@ts-ignore
+		window.getHighscores = getHighscores;
+		//@ts-ignore
+		window.getPlayerScore = getPlayerScore;
 	  }, []);
 
     return(
 		<div className="m-0 p-0 border-none text-center">
-			<button onClick={submitScore}>Press me</button>
 			<canvas id="canvas">
 				HTML5 canvas appears to be unsupported in the current browser.
 				Please try updating or use a different browser.
